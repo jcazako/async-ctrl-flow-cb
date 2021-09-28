6c9d43f246bcf4d383efe9c8b8e3f472d87f56bb
jcazako <jeancharles.cazako@gmail.com>
2021-09-28 13:12:13 +0200

    snd commit

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,2 +1,15 @@
-import { urlToFilename } from "../lib/utils.js";
+import { urlToFilename } from '../lib/utils.js'
+import { describe, it } from 'mocha'
+import { expect } from 'chai'
 
+/**
+ * Test the urlFileName function
+ */
+
+describe('Test urlToFilename',() => {
+  it('Should return correct filename', () => {
+    const url = 'https://loige.co'
+    const filename = urlToFilename(url)
+    expect(filename).to.be.equal('loige.co.html')
+  })
+})
