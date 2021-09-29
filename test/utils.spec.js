8af34e42456054b8e331fbf60f529e4c73c1cee1
jcazako <jeancharles.cazako@gmail.com>
2021-09-29 17:18:54 +0200

    start writting test for saving and downloading functionalities

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,15 +1,35 @@
-import { urlToFilename } from '../lib/utils.js'
-import { describe, it } from 'mocha'
+import { urlToFilename, save } from '../lib/utils.js'
+import { describe, it, before, after } from 'mocha'
 import { expect } from 'chai'
 
-/**
- * Test the urlFileName function
- */
+const { log, error } = console
 
-describe('Test urlToFilename',() => {
-  it('Should return correct filename', () => {
+describe('Functional test',() => {
+  it('urlFileName function should return a correct filename', () => {
     const url = 'https://loige.co'
     const filename = urlToFilename(url)
     expect(filename).to.be.equal('loige.co.html')
+  })
+
+  describe('File saving test', () => {
+    before(() => {
+      // const url = 'https://loige.co'
+      // const filename = urlToFilename(url)
+      // save(filename, 'CONTENT TEST', (err, filename, downloaded) => {
+      //   if (err) return error(err)
+      //   if (downloaded) return log(`saved "${filename}"`)
+
+      // })
+      log('BEFORE')
+    })
+
+    after(() => {
+      log('AFTER')
+    })
+    it('Should save function should save file correctly')
+  })
+
+  describe('Download content test', () => {
+    it('Should download content from url and save the file correctly')
   })
 })
