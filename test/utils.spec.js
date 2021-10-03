40b31451428c9ef89445c944dab5f4356ad03dd3
jcazako <jeancharles.cazako@gmail.com>
2021-10-03 16:01:07 +0200

    adding some test and starting to code spiderV2 utils

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,9 +1,13 @@
-import { urlToFilename, save } from '../lib/utils.js'
-import { describe, it, after } from 'mocha'
+import { urlToFilename, 
+  save,
+  download, 
+  getLinkUrl,
+  getPageLinks } from '../lib/utils.js'
+import { describe, it, afterEach } from 'mocha'
 import { expect } from 'chai'
 import fs from 'fs'
 
-const { error } = console
+const { error, log } = console
 
 describe('Functional test',() => {
   const url = 'https://loige.co'
@@ -26,9 +30,40 @@ describe('Functional test',() => {
       })
     })
 
-    after(() => {
+    it('Should download content properly from url', (done) => {
+      download(url, filename, (err, data) => {
+        if (err) done(err)
+        expect(data).to.not.be.undefined
+        done()
+      })
+    })
+
+    afterEach(() => {
       fs.unlink(filename, err => {
         if (err) return error(err)
+        log(`Deleted ${filename}`)
+      })
+    })
+  })
+
+  describe('', () => {
+    it('Should get url links', () => {
+      const parsedLinks = getLinkUrl(url, 'a')
+      expect(parsedLinks).to.not.be.undefined
+    })
+  
+    it('Should parse body links', (done) => {
+      fs.readFile('./test/loige.co.for.test.html', (err, data) => {
+        if (err) done(err)
+        const pageLinks = getPageLinks(url, data)
+        expect(pageLinks).to.not.be.undefined
+        expect(pageLinks).to.be.an('array')
+        pageLinks.forEach(link => {
+          expect(link).to.not.be.null
+          expect(link).to.be.an('string')
+          expect(link).to.not.be.empty
+        })
+        done()
       })
     })
   })
